0b1a41eedf62fd7510dd4b063148262063df9454
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-07-29 07:02:14 -0400

    Adds basic route patterns.

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -5,10 +5,36 @@ window.TD = {
   Routers: {},
   
   initialize: function ($rootEl, tasks) {
-    var tasksListView = new TD.Views.TasksListView({
-      collection: tasks
+    new TD.Routers.TasksRouter($rootEl, tasks);
+    Backbone.history.start();
     });
     
     $rootEl.html(tasksListView.render().$el);
   }
 };
+
+TD.Routers.TasksRouter = Backbone.Router.extend({
+  initialize: function($rootEl, tasks) {
+    this.$rootEl = $rootEl;
+    this.tasks = tasks;
+  },
+  
+  routes: {
+    "": "index",
+    "tasks/:id": "show"
+  },
+  
+  index: function () {
+    var that = this
+    
+    var tasksListView = new TD.Views.TasksListView({
+      collection: that.tasks
+    });
+    
+    that.$rootEl.html(tasksListView.render().$el);
+    
+    show: function (id) {
+      console.log("I'm just getting warmed up!");
+    }
+  }
+});
